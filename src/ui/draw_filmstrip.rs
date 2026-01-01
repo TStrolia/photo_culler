9d2c9d4f04654174f227caed349731f45ba642ae
Thornton Strolia <thorntonstrolia@gmail.com>
2026-01-01 17:16:25 -0500

    fixed ui.horizontal

--- src/ui/draw_filmstrip.rs
+++ src/ui/draw_filmstrip.rs
@@ -5,12 +5,14 @@ pub fn draw_filmstrip(ui: &mut Ui, images: &[PathBuf]) {
     ui.heading("Filmstrip");
     
     egui::ScrollArea::horizontal().show(ui, |ui| {
-        for path in images {
-            let name = path.file_name()
-                .map(|n| n.to_string_lossy())
-                .unwrap_or_else(|| "???".into());
+        ui.horizontal(|ui| {
+            for path in images {
+                let name = path.file_name()
+                    .map(|n| n.to_string_lossy())
+                    .unwrap_or_else(|| "???".into());
 
-            ui.button(name);
-        }
+                ui.button(name);
+            }
+        });
     });
 }
