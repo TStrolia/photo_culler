8795715a2de3ceaf1e99e78c4ed3d63516d67f3f
Thornton Strolia <thorntonstrolia@gmail.com>
2026-01-01 16:58:20 -0500

    update app state for incoming pictures

---
+++ src/io/find_images.rs
@@ -0,0 +1,24 @@
+use std::{fs, path::{Path, PathBuf}};
+
+pub fn find_images(dir: &Path) -> Vec<PathBuf> {
+    let mut images = Vec::new();
+
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entris.flatten() {
+            let path =  entry.path();
+
+            if let Some(extension) = path.extension() {
+                if let Some(ext_str) = extension.to_str() {
+                    let ext = ext_str.to_lowercase();
+                    if ext == "jpg" || ext == "jpeg" || ext == "png" {
+                        images.push(path);
+                    }
+                }
+            }
+        }
+    }
+
+    images.sort();
+
+    images
+}
--- src/state.rs
+++ src/state.rs
@@ -1,11 +1,26 @@
 use eframe::{App, Frame};
 use egui::Context;
+use std::path::PathBuf;
 
 use crate::ui;
+use crate::io;
 
 #[derive(Default)]
 pub struct PhotoCullerApp {
-    //
+    pub current_path: PathBuf,
+    pub images: Vec<PathBuf>,
+}
+
+impl Default for PhotoCullerApp {
+    fn default() -> Self {
+        let start_path = PathBuf::from(".");
+        let found_images = io::find_images::find_images(&start_path);
+
+        self {
+            current_path: start_path,
+            images: found_images,
+        }
+    }
 }
 
 impl App for PhotoCullerApp {
