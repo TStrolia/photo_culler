21f81115e46c7ca44a0753934276ed37d28360bc
Thornton Strolia <thorntonstrolia@gmail.com>
2026-01-01 22:32:24 -0500

    fixed image display

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,5 @@
 use eframe::{App, Frame};
+use egui::epaint::tessellator::path;
 use egui::{Context, TextureHandle};
 use std::path::PathBuf;
 
@@ -28,21 +29,13 @@ impl Default for PhotoCullerApp {
 
 impl App for PhotoCullerApp {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
-        let mut target_path = self.current_path.clone();
+        let previous_path = self.current_path.clone();
+        let previous_selection = self.selected_image.clone();
 
         egui::SidePanel::left("tree").show(ctx, |ui| {
             let root = PathBuf::from(".");
-            ui::draw_tree::draw_tree(ui, &root, &mut target_path);
+            ui::draw_tree::draw_tree(ui, &root, &mut self.current_path);
         });
-
-        if target_path != self.current_path {
-            self.current_path = target_path;
-
-            self.selected_image = None;
-            self.current_texture = None;
-
-            self.images = io::find_images::find_images(&self.current_path)
-        }
 
         egui::TopBottomPanel::bottom("filmstrip").show(ctx, |ui| {
             ui::draw_filmstrip::draw_filmstrip(ui, &self.images, &mut self.selected_image);
@@ -56,19 +49,26 @@ impl App for PhotoCullerApp {
             ui::draw_viewer::draw_viewer(ui, &self.current_texture);
         });
 
-        let mut new_selection = self.selected_image.clone();
+        if self.current_path != previous_path {
 
-        if new_selection != self.selected_image {
-            self.selected_image = new_selection;
+            self.selected_image = None;
+            self.current_texture = None;
 
+            self.images = io::find_images::find_images(&self.current_path)
+        }
+
+        if self.selected_image != previous_selection {
             if let Some(path) = &self.selected_image {
                 if let Some(color_image) = io::load_image::load_image(path) {
                     self.current_texture = Some(ctx.load_texture(
                         "active_image", 
-                        color_image, 
-                    Default::default()
-                ))
+                        color_image,
+                        Default::default()
+                    ));
                 }
+            }
+            else {
+                self.current_texture = None;
             }
         }
     }
