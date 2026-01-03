faace76538955b58b618bc1ca836d588d3460bde
Thornton Strolia <thorntonstrolia@gmail.com>
2026-01-03 16:10:50 -0500

    revisions to filmstrip_frame.rs fn film_frame

--- src/ui/components/buttons/filmstrip_frame.rs
+++ src/ui/components/buttons/filmstrip_frame.rs
@@ -1,67 +1,78 @@
-use egui::Ui;
-use eframe::egui;
-use egui::{Frame, Sense, UiBuilder};
+use eframe::{egui, glow::LINE_SMOOTH};
+use egui::{Frame, LayerId, Order, Sense, Stroke, Ui, response};
 
 pub fn film_frame(
     ui: &mut Ui,
     image: egui::Image<'static>,
     text: &str,
     height: f32,
+    selected: bool,
 ) -> egui::Response {
     const PADDING: f32 = 4.0;
     const RADIUS: f32 = 4.0;
     const IMAGE_HEIGHT: f32 = 128.0;
 
-    let id = format!("{:?}_frame_btn",text);
+    let content_response = Frame::default()
+        .inner_margin(PADDING)
+        .show(ui, |ui| {
+            ui.set_height(height - (PADDING * 2.0));
+            ui.set_min_width(94.0 - (PADDING * 2.0));
+            ui.set_max_width(250.0 - (PADDING * 2.0));
 
-    ui.scope_builder(
-        UiBuilder::new()
-            .id_salt(id)
-            .sense(Sense::click()),
-        |ui| {
-            let (bg_fill, border_stroke, text_color) = {
-                let response = ui.response();
-
-                let visuals = ui.style().interact(&response);
-
-                let border_stroke = egui::Stroke::new(2.0, visuals.bg_stroke.color);
-                let bg_fill = if response.hovered() {
-                    visuals.bg_fill.linear_multiply(1.2)
-                } else {
-                    visuals.bg_fill
-                };
-
-                (bg_fill, border_stroke, visuals.text_color())
-            };
-            
-
-            Frame::default()
-                .fill(bg_fill)
-                .stroke(border_stroke)
-                .corner_radius(RADIUS)
-                .inner_margin(PADDING)
-                .show(ui, |ui| {
-                    ui.set_height(height - (PADDING * 2.0));
-                    ui.set_min_width(94.0 - (PADDING * 2.0));
-                    ui.set_max_width(250.0 - (PADDING * 2.0));
-
-                    ui.vertical(|ui| {
-                        ui.centered_and_justified(|ui| {
-                            ui.add(
-                                image.corner_radius(0.0).max_height(IMAGE_HEIGHT)
-                            );
-                        });
-
-                        ui.vertical_centered(|ui| {
-                            ui.label(
-                                egui::RichText::new(text)
-                                    .color(text_color)
-                                    .size(11.0)
-                            )
-                        });
-                    });
+            ui.vertical(|ui| {
+                ui.centered_and_justified(|ui| {
+                    ui.add(
+                image.max_height(IMAGE_HEIGHT).corner_radius(0.0)
+                    );
                 });
-        },
-    )
-    .response
+
+                ui.vertical_centered(|ui|{
+                    ui.label(
+                        egui::RichText::new(text)
+                            .size(12.0)
+                            .color(if selected {
+                                ui.visuals().strong_text_color()
+                            } else {
+                                ui.visuals().text_color()
+                            })
+                    )
+                });
+            });
+        })
+        .response;
+
+    let rect = content_response.rect;
+
+    let id = ui.make_persistent_id(text);
+    let response = ui.interact(rect, id, Sense::click());
+
+    let visuals = ui.style().interact_selectable(&response, selected);
+
+
+    let bg_fill = if selected {
+        ui.visuals().selection.bg_fill
+    } else if response.hovered() {
+        ui.visuals().widgets.hovered.bg_fill
+    } else {
+        ui.visuals().faint_bg_color
+    };
+
+
+    let border_stroke = if selected {
+        ui.visuals().selection.stroke
+    } else if response.hovered() {
+        ui.visuals().widgets.hovered.fg_stroke
+    } else {
+        Stroke::new(1.0, ui.visuals().widgets.noninteractive.bg_stroke.color)
+    };
+
+
+    ui.ctx().layer_painter(LayerId::new(Order::Background, ui.id())).rect(
+        rect,
+        RADIUS,
+        bg_fill,
+        border_stroke,
+    );
+
+    response
 }
